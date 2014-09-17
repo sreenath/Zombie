47a777f498a621c540bb7739a510d60a58b812d1
Sreenath T V <sree.harishree@gmail.com>
2014-09-17 12:52:57 +1000

    Fixed Human and Zombie Movement

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <stdlib.h>
 #include <omp.h>
+#include <unistd.h>
+#include "Occupant.cpp"
 using namespace std;
 
 #define STEPS	100
-#define SIZE	100
-#define MOVE	0.1
+#define SIZE 	100
+
+bool isEmpty(Occupant o) {
+	if (o.probabilityOfMovement == 0.0) {
+		return true;
+		} 
+		return false;
+}
 
 #if defined(_OPENMP)
 void lock(int i, bool *locks) {
@@ -27,11 +35,14 @@ void unlock(int i, bool *locks) {
 }
 #endif
 
-bool **CreateMesh(int I, int J) {
-	bool **Mesh = new bool *[I];
+Occupant **CreateMesh(int I, int J) {
+	Occupant **Mesh = new Occupant *[I];
 	for (int i = 0; i < I; i++) {
-		Mesh[i] = new bool[J];
-		for (int j = 0; j < J; j++) Mesh[i][j] = false;
+		Mesh[i] = new Occupant[J];
+		for (int j = 0; j < J; j++) {
+		  	
+			//Mesh[i][j] = NULL;	
+		}
 	}
 	return(Mesh);
 }
@@ -39,118 +50,140 @@ bool **CreateMesh(int I, int J) {
 /* 
  * Boundary condition check using combination of No Flux & Periodic Approach 
  */
+
  
-bool **checkBoundary(bool **Mesh) {
+Occupant **checkBoundary(Occupant **Mesh) {
+	Occupant temp;
 	for (int i = 1; i <= SIZE; i++) {
-		if(Mesh[i][0] == true) {
-			Mesh[i][0] = false;
-			if (Mesh[i][1] == false) {
-				Mesh[i][1] = true;
+		if(!isEmpty(Mesh[i][0])) {
+			if (isEmpty(Mesh[i][1])) {
+				Mesh[i][1] = Mesh[i][0];
 			} else {
-				Mesh[i][SIZE] = true;
+				Mesh[i][SIZE] = Mesh[i][0];
 			}
+			Mesh[i][0] = temp;
 		}
 		
-		if(Mesh[i][SIZE + 1] == true) {
-			Mesh[i][SIZE + 1] = false;
-			if (Mesh[i][SIZE] == false) {
-				Mesh[i][SIZE] = true;
+		if(!isEmpty(Mesh[i][SIZE + 1])) {
+			if (isEmpty(Mesh[i][SIZE])) {
+				Mesh[i][SIZE] = Mesh[i][SIZE + 1];
 			} else {
-				Mesh[i][1] = true;
+				Mesh[i][1] = Mesh[i][SIZE + 1];
 			}
+			Mesh[i][SIZE + 1] = temp;
 		}
 		
-		if(Mesh[0][i] == true) {
-			Mesh[0][i] = false;
-			if (Mesh[1][i] == false) {
-				Mesh[1][i] = true;
+		if(!isEmpty(Mesh[0][i])) {
+			if (isEmpty(Mesh[1][i])) {
+				Mesh[1][i] = Mesh[0][i];
 			} else {
-				Mesh[SIZE][i] = true;
+				Mesh[SIZE][i] = Mesh[0][i];
 			}
+			Mesh[0][i] = temp;
 		}
 
-		if(Mesh[SIZE + 1][i] == true) {
-			Mesh[SIZE + 1][i] = false;
-			if (Mesh[SIZE][i] == false) {
-				Mesh[SIZE][i] = true;
+		if(!isEmpty(Mesh[SIZE + 1][i])) {
+			if (isEmpty(Mesh[SIZE][i])) {
+				Mesh[SIZE][i] = Mesh[SIZE + 1][i];
 			} else {
-				Mesh[1][i] = true;
+				Mesh[1][i] = Mesh[SIZE + 1][i];
 			}
+			Mesh[SIZE + 1][i] = temp;
 		} 
 	}
 	return(Mesh);
 }
-
-void print(bool **Mesh, int t) {
-	int populationTotal = 0;
+		
+void print(Occupant **Mesh, int t) {
+	int humanPop = 0;
+	int zombiePop = 0;
          for (int i = 1; i <= SIZE; i++) {
                         for (int j = 1; j <= SIZE; j++) {
-				if (Mesh[i][j] == true) {
-					populationTotal++;	
+				if (Mesh[i][j].probabilityOfMovement == 0.1 ) {
+					humanPop++;	
+				}
+				else if (Mesh[i][j].probabilityOfMovement == 0.075 ){
+					zombiePop++;
 				}
 			}
 	}
-	cout<<t<<"\t"<<populationTotal<<std::endl;
+	cout<<t<<"\t"<<humanPop<<"\t"<<zombiePop<<std::endl;
 }
 
 int main(int argc, char **argv) {
-	srand48(8767134);
+	srand48(getpid());
 	bool *locks = new bool[SIZE + 2];
 	for (int i = 0; i < SIZE + 2; i++) locks[i] = false;
-	bool **MeshA = CreateMesh(SIZE + 2, SIZE + 2);
-	bool **MeshB = CreateMesh (SIZE + 2, SIZE + 2);
+	Occupant **MeshA = CreateMesh(SIZE + 2, SIZE + 2);
+	Occupant **MeshB = CreateMesh (SIZE + 2, SIZE + 2);
 
 	for (int i = 1; i <= SIZE; i++) {
 		for (int j = 1; j <= SIZE; j++) {
-			if (drand48() < 0.10) {
+			//#zombies should be reduced
+			if (drand48() < 0.01) {
 				// Modification required to incorporate Male, Female & Zombie differentiation
-				MeshA[i][j] = true;
+				Zombie z;
+				MeshA[i][j] = z;
+			}
+			else if(drand48()<0.10){
+				Human h;
+				MeshA[i][j] = h;
 			}
 		}
 	}
 
-        std::cout<<"Time"<<"\t"<<"population"<<std::endl;	
+        std::cout<<"Time"<<"\t"<<"humanPop"<<"\t"<<"zombiePop"<<std::endl;	
 	print(MeshA, 0);
+
 	for (int n = 0; n < STEPS; n++) {
 		#if defined(_OPENMP)
 		#pragma omp parallel for default(none) shared(MeshA, MeshB, locks, n)
 		#endif
+	
 		for (int i = 1; i <= SIZE; i++) {
 			lock(i, locks);
-			for (int j = 1; j <= SIZE; j++) if (MeshA[i][j] == true) { 
-				// Modification required to differentiate movement of human & zombie
-				MeshA[i][j] = false;
-				double move = drand48();
-				if (move < 1.0*MOVE && MeshB[i-1][j] == false && MeshA[i-1][j] == false) {
-					MeshB[i-1][j] = true;
-				} else if (move < 2.0*MOVE && MeshB[i+1][j] == false && MeshA[i+1][j] == false) {
-					MeshB[i+1][j] = true;
-				} else if (move < 3.0*MOVE && MeshB[i][j-1] == false && MeshA[i][j-1] == false) {
-					MeshB[i][j-1] = true;
-				} else if (move < 4.0*MOVE && MeshB[i][j+1] == false && MeshA[i][j+1] == false) {
-					MeshB[i][j+1] = true;
-				} else {
-					MeshB[i][j] = true;
+			for (int j = 1; j <= SIZE; j++) {
+				if (!isEmpty(MeshA[i][j])) { 
+					double move = drand48();
+					if (move < 1.0*MeshA[i][j].probabilityOfMovement && isEmpty(MeshB[i-1][j]) && isEmpty(MeshA[i-1][j])) {
+						MeshB[i-1][j] = MeshA[i][j];
+					} else if (move < 2.0*MeshA[i][j].probabilityOfMovement && isEmpty(MeshB[i+1][j]) && isEmpty(MeshA[i+1][j])) {
+							MeshB[i+1][j] = MeshA[i][j];
+					} else if (move < 3.0*MeshA[i][j].probabilityOfMovement && isEmpty(MeshB[i][j-1]) && isEmpty(MeshA[i][j-1])) {
+							MeshB[i][j-1] = MeshA[i][j];
+					} else if (move < 4.0*MeshA[i][j].probabilityOfMovement && isEmpty(MeshB[i][j+1]) && isEmpty(MeshA[i][j+1])) {
+							MeshB[i][j+1] = MeshA[i][j];
+					} else {
+							MeshB[i][j] = MeshA[i][j];
+					}
+				
+					Occupant temp;
+					MeshA[i][j] = temp;	
 				}
-			} 
+		} 
 			unlock(i, locks);
 		}
 
 		MeshB = checkBoundary(MeshB);		
 		swap(MeshA, MeshB);
-		print(MeshA, n+1);		
+		print(MeshA, n+1);	
+
 /*
+		cout<< "\n\nMesh A \n";
 		for(int p = 0; p <= SIZE; p++) {
 			for(int q = 0; q <= SIZE; q++) {
-				if(MeshA[p][q] == true) {
-					cout<<"X | ";
-				} else {
-					cout<<"0 | ";
+				if(MeshA[p][q].probabilityOfMovement == 0.1 ) {
+					cout<<"H | ";
+				} else if (MeshA[p][q].probabilityOfMovement == 0.075) {
+					cout<<"Z | ";
+				}else{
+					cout<<"  | ";
 				}
 			}
 			cout<<"\n";
 		}
-		cout<<"\n\n\n\n\n\n\n\n\n";*/
+		cout<<"\n\n\n\n\n\n\n\n\n";
+*/
 	}
 }
 
--- Occupant.cpp
+++ Occupant.cpp
@@ -6,31 +6,36 @@ class Occupant{
    
 	public:
 		double probabilityOfMovement;
-		
-	//	Occupant(double p) {
-	//		probabilityOfMovement = p;
-	//	}
 
+		Occupant() {
+			probabilityOfMovement = 0.0;	
+		}
 };
 
 class Human: public Occupant{
+	public :
+		Human(){
+			probabilityOfMovement = 0.1;
 
+		}
 };
 class Zombie: public Occupant{
-
+	public :
+		Zombie(){
+			probabilityOfMovement = 0.075;
+		}
 };
 
 /**int main(){	
- Occupant oc ;
+ Occupant oc;
  Human h;
  Zombie z;
- oc.probabilityOfMovement = 0.333;
  h.probabilityOfMovement = 0.7;
  z.probabilityOfMovement = 0.4;
  cout << oc.probabilityOfMovement<<endl;
  cout << h.probabilityOfMovement<<endl;
  cout << z.probabilityOfMovement<<endl;
-
+ return 0;
 }**/
 
 
